5a7b52cc34267edbb14a8d5b6c7fb176cd34f9ea
Kian <116115355+Kian-2108@users.noreply.github.com>
2023-09-04 08:06:22 +0000

    .

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ import pandas as pd
 df = pd.read_csv('New HIV infections_New HIV infections - All ages_Population_ All ages.csv')
 country_list = df["Country"]
 country_name = st.selectbox("Country",country_list)
+
+st.dataframe(df[df["Country"]==country_name])
