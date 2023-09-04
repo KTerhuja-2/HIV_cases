4e21aa92885fb139482773890e258cbb6bc7b0d2
Kian <116115355+Kian-2108@users.noreply.github.com>
2023-09-04 11:29:12 +0000

    .

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ country_list = [
     'Namibia', 'Niger', 'Nigeria', 'Rwanda', 'Sao Tome and Principe', 'Senegal', 'Sierra Leone', 'Somalia', 
     'South Africa', 'South Sudan', 'Sudan', 'Tanzania', 'Togo', 'Tunisia', 'Uganda', 'Zambia', 'Zimbabwe'
 ]
+
 country_name = st.selectbox("Country",country_list)
 input_years = st.slider("Forecast Period",1,10)
-# st.dataframe(df[[country_name]])
-# st.dataframe(utils.predict(df,country_name,input_years))
+
 plot_df = utils.combine(df[[country_name]],utils.predict(df,country_name,input_years)).rename(columns={country_name:"HIV Population"})
-# st.dataframe(plot_df)
+
 fig = px.line(plot_df,x=plot_df.index,y="HIV Population",color="Tag",title=f"HIV Population in {country_name}")
 fig.update_layout(
     xaxis_title="Year", yaxis_title="HIV Population"
 )
-st.plotly_chart(fig)
 
+st.plotly_chart(fig)
