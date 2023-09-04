bdd11192e9d5117bd3ef40ca27aa6ca5f3e6e9f1
Kian <116115355+Kian-2108@users.noreply.github.com>
2023-09-04 10:47:09 +0000

    .

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ df = pd.read_csv("HIV_data 1990-2022.csv",index_col=0).rename_axis("Country")
 country_list = df.columns
 country_name = st.selectbox("Country",country_list)
 input_years = st.slider("Forecast Period",1,10)
-plot_df = utils.combine(df[[country]],utils.predict(df,country_name,input_years))
-fig = px.line(pdf,x=pdf.index,y=country,color="Tag")
+plot_df = utils.combine(df[[country_name]],utils.predict(df,country_name,input_years))
+fig = px.line(pdf,x=pdf.index,y=country_name,color="Tag")
 st.plotly_chart(fig)
