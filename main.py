09096d23f4d0d52c82a895d7f4b1537a74ae2b34
Kian <116115355+Kian-2108@users.noreply.github.com>
2023-09-04 20:38:45 +0000

    .

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import utils
 
-st.set_page_config(layout="wide")
+st.set_page_config(page_title="HIV Cases",layout="wide")
 
 df = pd.read_csv("HIV_data 1990-2022.csv",index_col=0).dropna(axis=1)
 country_list = [
