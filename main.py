7529d53674bfb21fa263b67885287bc3e753e97a
Kian <116115355+Kian-2108@users.noreply.github.com>
2023-09-04 10:23:32 +0000

    updated chart

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import utils
 
 
 df = pd.read_csv("HIV_data 1990-2022.csv",index_col=0).rename_axis("Country")
 country_list = df.columns
 country_name = st.selectbox("Country",country_list)
-pdf = utils.predict(df,country_name,5)
+input_years = st.slider("Forecast Period",1,10)
+pdf = utils.predict(df,country_name,input_years)
 fig = px.line(pdf,x=pdf.index,y="HIV Population",color="History")
 st.plotly_chart(fig)
