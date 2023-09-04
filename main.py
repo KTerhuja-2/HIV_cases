4797d00a79a6f33a87b6c5b9bd9b65d8cc88a51b
Kian <116115355+Kian-2108@users.noreply.github.com>
2023-09-04 10:22:08 +0000

    updated main.py

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import plotly.express as px
 df = pd.read_csv("HIV_data 1990-2022.csv",index_col=0).rename_axis("Country")
 country_list = df.columns
 country_name = st.selectbox("Country",country_list)
-
-st.dataframe(df[df["Country"]==country_name])
-
 pdf = utils.predict(df,country_name,5)
 fig = px.line(pdf,x=pdf.index,y="HIV Population",color="History")
 st.plotly_chart(fig)
