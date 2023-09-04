3c15c2faab426a09c6756ee85018337ed5516c6c
Kian <116115355+Kian-2108@users.noreply.github.com>
2023-09-04 08:05:56 +0000

    updated main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import streamlit as st
+import pandas as pd
 
+
+df = pd.read_csv('New HIV infections_New HIV infections - All ages_Population_ All ages.csv')
+country_list = df["Country"]
+country_name = st.selectbox("Country",country_list)
