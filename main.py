c27151403e831caefb2afb3c79870da9ad71c378
Kian <116115355+Kian-2108@users.noreply.github.com>
2023-09-04 20:39:52 +0000

    .

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import utils
 
 st.set_page_config(page_title="HIV Cases",layout="wide")
 
-st.title("New HIV Cases Forecast")
+st.title("New HIV Cases Forecast in Africa")
 
 df = pd.read_csv("HIV_data 1990-2022.csv",index_col=0).dropna(axis=1)
 country_list = [
