88cc6cf2f80e66eb9d4499a7e70fe794abf928c5
Kian <116115355+Kian-2108@users.noreply.github.com>
2023-09-04 12:47:59 +0000

    .

--- main.py
+++ main.py
@@ -42,6 +42,6 @@ show_df["Year"] = show_df["Year"].astype("object")
 show_df["New HIV Population"] = show_df["New HIV Population"].astype("int")
 l.dataframe(show_df.style.format(thousands=''),use_container_width=True)
 
-map_df = df.transpose()[[2022]].copy()
+map_df = df[sorted(set(country_list).intersection(df.columns))].transpose()[[2022]].copy()
 map_df["ISO"] = utils.country_iso_alpha3
 st.dataframe(map_df)
