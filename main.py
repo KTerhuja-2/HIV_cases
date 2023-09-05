b081b971a211f78b73cf0b280e70bfb0ee0672d8
Kian <116115355+Kian-2108@users.noreply.github.com>
2023-09-05 04:24:06 +0000

    .

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ l.dataframe(show_df.style.format(thousands=''),use_container_width=True)
 
 
 map_df = pd.read_csv("HIV_data 1990-2032.csv",index_col=0).dropna(axis=1)
-map_df = map_df[sorted(set(country_list).intersection(df.columns))].transpose()[[input_years]].copy()
+map_df = map_df[sorted(set(country_list).intersection(df.columns))].transpose()[[input_years]].astype("int").copy()
 map_df["ISO"] = utils.country_iso_alpha3
 fig2 = px.choropleth(
     map_df, 
